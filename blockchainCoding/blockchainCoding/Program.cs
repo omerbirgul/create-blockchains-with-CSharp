891d8a0a677aa46ccf130dd559623626e566f1e8
Omer Faruk Birgul <omerbirgul1@hotmail.com>
2024-02-07 00:37:55 +0300

    %51 attack trying

--- blockchainCoding/blockchainCoding/Program.cs
+++ blockchainCoding/blockchainCoding/Program.cs
@@ -16,6 +16,22 @@ namespace blockchainCoding
             ourBlockchain.AddBlock(new Block(DateTime.Now, null, "{sender: Micheal, receiver: Gustavo, amount: 50}"));
             ourBlockchain.AddBlock(new Block(DateTime.Now, null, "{sender: Haaland, receiver: Sasha, amount: 10}"));
             Console.WriteLine(JsonConvert.SerializeObject(ourBlockchain,Formatting.Indented));
+            Console.WriteLine("blokchain gecerli mi? " + ourBlockchain.IsValid().ToString());
+            Console.WriteLine("Veri degistiriliyor...");
+
+            //Butun blokların hash'i degisti(%51 attack). degisim olunca gecerlilik true olur.
+
+            ourBlockchain.Chain[1].Hash = ourBlockchain.Chain[1].CalculateHash();
+
+            ourBlockchain.Chain[2].PreviousHash = ourBlockchain.Chain[1].Hash;
+            ourBlockchain.Chain[2].Hash = ourBlockchain.Chain[2].CalculateHash();
+
+            ourBlockchain.Chain[3].PreviousHash = ourBlockchain.Chain[2].Hash;
+            ourBlockchain.Chain[3].Hash = ourBlockchain.Chain[3].CalculateHash();
+
+            Console.WriteLine("blokchain gecerli mi? " + ourBlockchain.IsValid().ToString());
+
+
             Console.ReadKey();
         }
     }
