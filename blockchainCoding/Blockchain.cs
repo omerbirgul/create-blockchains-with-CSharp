fae5c25cd331ecdc81c7181976252342208bad21
Omer Faruk Birgul <omerbirgul1@hotmail.com>
2024-02-07 00:11:57 +0300

    controlling the change of blocks

---
+++ blockchainCoding/Blockchain.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace blockchainCoding
+{
+    public class Blockchain
+    {
+        public IList<Block> Chain { get; set; }
+        public Blockchain() 
+        {
+            InitializeChain();
+            AddGenesisBlock();
+        }
+
+        public void InitializeChain()
+        {
+            Chain = new List<Block>();
+        }
+
+        public Block CreateGenesisBlock()
+        {
+            return new Block(DateTime.Now, null, "{}");
+        }
+
+        public void AddGenesisBlock()
+        {
+            Chain.Add(CreateGenesisBlock());
+        }
+
+        public Block GetLatestBlock()
+        {
+            return Chain[Chain.Count - 1];
+        }
+
+        public void AddBlock(Block block)
+        {
+            Block latestBlock = GetLatestBlock();
+            block.Index = latestBlock.Index + 1;
+            block.PreviousHash = latestBlock.Hash;
+            block.Hash = block.CalculateHash();
+            Chain.Add(block);
+        }
+
+        public bool IsValid()
+        {
+            for(int i = 1; i < Chain.Count; i++) 
+            {
+                Block currentBlock = Chain[i];
+                Block previosuBlock = Chain[i - 1];
+                if(currentBlock.Hash != currentBlock.CalculateHash())
+                {
+                    return false;
+                }
+                if(currentBlock.PreviousHash != previosuBlock.Hash) 
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ blockchainCoding/Program.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace blockchainCoding
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Blockchain ourBlockchain = new Blockchain();
+            ourBlockchain.AddBlock(new Block(DateTime.Now, null, "{sender: Omer, receiver: Duygu, amount: 5}"));
+            ourBlockchain.AddBlock(new Block(DateTime.Now, null, "{sender: Micheal, receiver: Gustavo, amount: 50}"));
+            ourBlockchain.AddBlock(new Block(DateTime.Now, null, "{sender: Haaland, receiver: Sasha, amount: 10}"));
+            Console.WriteLine(JsonConvert.SerializeObject(ourBlockchain,Formatting.Indented));
+            Console.WriteLine("gecerli mi? " + ourBlockchain.IsValid().ToString());
+            Console.WriteLine("Veri degistiriliyor...");
+            ourBlockchain.Chain[1].Data = "{sender: ABC, receiver: XYZ, amount: 100}";
+            Console.WriteLine("degistirildi ve gecerli mi? " + ourBlockchain.IsValid().ToString());
+            Console.ReadKey();
+        }
+    }
+}
