3225fa127c62f6f613a7af50d8328e0a4731c5c3
Omer Faruk Birgul <omerbirgul1@hotmail.com>
2024-02-07 23:51:19 +0300

    proof of work example added

---
+++ blockchainCoding/Block.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace blockchainCoding
+{
+    public class Block
+    {
+        public int Index { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public string PreviousHash { get; set; }
+        public string Hash { get; set; }
+        public string Data { get; set; }
+        public int Nonce { get; set; } = 0;
+
+        public Block(DateTime timeStamp, string previousHash, string data)
+        {
+            this.TimeStamp = timeStamp;
+            this.PreviousHash = previousHash;
+            this.Data = data;
+            this.Index = 0;
+            this.Hash = CalculateHash();
+        }
+
+
+        public string CalculateHash()
+        {
+            SHA256 sha256 = SHA256.Create();
+            byte[] inByte = Encoding.ASCII.GetBytes($"{TimeStamp}-{PreviousHash ?? ""}-{Data}-{Nonce}");
+            byte[] ourByte = sha256.ComputeHash( inByte );
+            return Convert.ToBase64String( ourByte );
+        }
+
+        public void Mine(int difficulty)
+        {
+            var leadingZeros = new String('0', difficulty);
+            while(this.Hash == null || this.Hash.Substring(0, difficulty) != leadingZeros)
+            {
+                this.Nonce++;
+                this.Hash = CalculateHash();
+
+            }
+        }
+    }
+}
---
+++ blockchainCoding/Program.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace blockchainCoding
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Blockchain ourBlockchain = new Blockchain();
+
+            DateTime startTime = DateTime.Now;
+            ourBlockchain.AddBlock(new Block(DateTime.Now, null, "{sender: Omer, receiver: Duygu, amount: 5}"));
+            ourBlockchain.AddBlock(new Block(DateTime.Now, null, "{sender: Micheal, receiver: Gustavo, amount: 50}"));
+            ourBlockchain.AddBlock(new Block(DateTime.Now, null, "{sender: Haaland, receiver: Sasha, amount: 10}"));
+
+            DateTime endTime = DateTime.Now;
+            Console.WriteLine($"Hesaplamalar icin gerekli sure: {(startTime - endTime).ToString()}");
+            Console.WriteLine(JsonConvert.SerializeObject(ourBlockchain,Formatting.Indented));
+            Console.WriteLine("blokchain gecerli mi? " + ourBlockchain.IsValid().ToString());
+            Console.WriteLine("Veri degistiriliyor...");
+
+            //Butun blokların hash'i degisti(%51 attack). degisim olunca gecerlilik true olur.
+
+            ourBlockchain.Chain[1].Hash = ourBlockchain.Chain[1].CalculateHash();
+
+            ourBlockchain.Chain[2].PreviousHash = ourBlockchain.Chain[1].Hash;
+            ourBlockchain.Chain[2].Hash = ourBlockchain.Chain[2].CalculateHash();
+
+            ourBlockchain.Chain[3].PreviousHash = ourBlockchain.Chain[2].Hash;
+            ourBlockchain.Chain[3].Hash = ourBlockchain.Chain[3].CalculateHash();
+
+            Console.WriteLine("blokchain gecerli mi? " + ourBlockchain.IsValid().ToString());
+
+
+            Console.ReadKey();
+        }
+    }
+}
